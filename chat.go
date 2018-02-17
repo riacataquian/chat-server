33efcb3963d360d3baf9f4e4a916cb7d2532d6ba
Ria Cataquian <icataquian@gmail.com>
2018-02-17 18:29:04 +0800

    Improve chat implementation.
    - [x] Handle idle client connections.
    - [x] Non-blocking broadcaster.
    - [x] Accept client username.

--- chat.go
+++ chat.go
@@ -6,39 +6,81 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
-type client chan<- string
+type client struct {
+	ch   chan<- string
+	name string
+}
 
 var (
-	messages = make(chan string) // all incoming client messages
-	entering = make(chan client) // handle arrival of new clients
-	leaving  = make(chan client) // handle leaving clients
+	messages    = make(chan string) // all incoming client messages
+	entering    = make(chan client) // handle arrival of new clients
+	leaving     = make(chan client) // handle leaving clients
+	doHeadcount = make(chan bool)   // all changes in client: arrival or leaving
+	cleanups    = make(chan bool)   // handle idle client disconnection
+
+	clients = make(map[client]bool)   // client set
+	idle    = make(map[net.Conn]bool) // determine if client is idle
 )
 
-func broadcast() {
-	clients := make(map[client]bool) // client set
+// disconnectIdle disconnects a client if idle for 5 minutes.
+// It performs disconnection after a signal has been received from the cleanups channel.
+// After the cleanups, it restarts the timer.
+func disconnectIdle(conn net.Conn) {
+	for {
+		select {
+		case <-cleanups:
+			if c, ok := idle[conn]; ok && c {
+				conn.Close()
+			}
 
+			go idleCounter()
+		}
+	}
+}
+
+// idleCounter spawn a goroutine that sends a signal to the cleanups channel
+// every 5 minutes.
+func idleCounter() {
+	go func() {
+		time.Sleep(5 * time.Minute) // 5 mins
+		cleanups <- true
+	}()
+}
+
+func broadcast() {
 	for {
 		// Multiplex using select.
 		select {
 		case msg := <-messages:
 			for c := range clients {
-				c <- msg
+				// Delegate to another goroutine, so this channel would be non-blocking.
+				// Otherwise, if client is blocked, this will cause a goroutine leak.
+				go send(c, msg)
 			}
 
 		case c := <-entering:
 			clients[c] = true
+			go idleCounter()
+			doHeadcount <- true
 
 		case c := <-leaving:
 			delete(clients, c)
-			close(c)
+			close(c.ch)
+			doHeadcount <- true
 
 		}
 	}
 }
 
-// main creates a TCP chat server.
+// send sends a message to the client.
+func send(c client, msg string) {
+	c.ch <- msg
+}
+
+// Creates a chat server
 func main() {
 	listener, err := net.Listen("tcp", "localhost:8000")
 	if err != nil {
@@ -59,6 +101,28 @@ func main() {
 
 		// Handle new request.
 		go handleConn(conn)
+		// Performs headcount for the active clients.
+		go headCount()
+		// Listen to cleanups channel.
+		go disconnectIdle(conn)
+	}
+}
+
+// headCount lists and reports all active clients.
+func headCount() {
+	for {
+		select {
+		case <-doHeadcount:
+			var msg string
+
+			msg = "\nPerforming headcount.."
+
+			for c := range clients {
+				msg += "\n" + c.name
+			}
+
+			messages <- msg
+		}
 	}
 }
 
@@ -66,32 +130,45 @@ func main() {
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 
+	// Returns a new scanner that reads from client's connection.
+	input := bufio.NewScanner(conn)
+
+	// Write in client's connection.
+	fmt.Fprint(conn, "Chat username: ")
+
+	var who string
+	if input.Scan() {
+		who = input.Text()
+	}
+
 	// create client its own channel
 	ch := make(chan string)
 	go clientWriter(conn, ch)
 
 	// Inform client of its identity
-	who := conn.RemoteAddr().String()
-	ch <- "You are " + who
+	ch <- "Joining as " + who
 
 	// Announce all existing clients that `who` joined.
 	// Note that this comes first, so newly joined clients
 	// won't receive broadcast of its own arrival.
 	messages <- who + " joined"
 
+	c := client{ch, who}
+
+	idle[conn] = true
+
 	// Then add the client channel to the clients set
 	// by sending an event to the `entering` channel.
-	entering <- ch
+	entering <- c
 
 	// Wait for client's input..
-	input := bufio.NewScanner(conn)
 	for input.Scan() {
-		// NOTE: ignoring err from input.Text()
-		messages <- who + ":" + input.Text()
+		messages <- who + ": " + input.Text()
+		idle[conn] = false
 	}
 
 	// This code is unreachable unless input.Scan() returns an error.
-	leaving <- ch
+	leaving <- c
 
 	// Broadcast leaving of clients.
 	messages <- who + " is leaving"
